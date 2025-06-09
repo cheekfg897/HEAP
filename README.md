113705adf57eb22ee2201afd3e8240ee6bc95cdc
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-06-09 13:52:53 +0800

    Added Resend to send a test email

--- README.md
+++ README.md
@@ -5,4 +5,13 @@
 2. Create a virtual environment and activate it.
    ```bash
    python -m venv venv
-   source venv/bin/activate
+   venv\scripts\activate
+3. Install dependencies
+   ```bash
+   pip install -r requirements.txt
+4. Create a .env file in the project root and add your Resend API key
+   ```bash
+   RESEND_API_KEY=your_resend_api_key_here
+5. Run the Flask app
+   ```bash
+  python app.py
--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,11 +1,18 @@
+import resend
 from flask import Flask, render_template, request, jsonify, Response
 import cv2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+resend.api_key = os.getenv("RESEND_API_KEY") 
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)  # Webcam
 detector = cv2.QRCodeDetector()
 qr_data = ""
 
+#opencv camera
 def generate_frames():
     global qr_data
     while True:
@@ -27,19 +34,35 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# Home route
+#load index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#generate live feed on frontend
 @app.route('/video')
 def video():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+#index.html fetching for qr data from backend (if any)
 @app.route('/attendance_status')
 def get_data():
     return jsonify({"data": qr_data})
+
+
+#function that uses Resend to send email
+@app.route("/send_email")
+def send_email():
+    params: resend.Emails.SendParams = {
+        "from": "Acme <onboarding@resend.dev>",
+        "to": ["Enter Your email you signup a resend account with here"],
+        "subject": "hello world",
+        "html": "<strong>it works!</strong>",
+    }
+
+    r = resend.Emails.send(params)
+    return jsonify(r)
 
 # Run the app
 if __name__ == '__main__':
