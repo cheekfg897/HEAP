00ff195e174177ae9869c34ff539e6721d04e251
cheekfg897 <gyaltsen.c.2024@computing.smu.edu.sg>
2025-06-13 10:45:59 +0800

    Update connection.py

--- flask_backend/supabase/connection.py
+++ flask_backend/supabase/connection.py
@@ -4,8 +4,8 @@ load_dotenv()
 import os
 from supabase import create_client
 
-url = os.environ.get("https://culnykmmgfetgpuglvrt.supabase.co")
-key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1bG55a21tZ2ZldGdwdWdsdnJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTgwMzcsImV4cCI6MjA2NTIzNDAzN30.q2eGpMlEkhAj32na-U7No_7ffNGv_HgECLpn1GCLyR0")
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
 
 supabase = create_client(url, key)
 
