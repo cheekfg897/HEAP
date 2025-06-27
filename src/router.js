759f1f53a93b2678090d13439defb34506ab5109
luxuan05 <luxuan.wee.2024@computing.smu.edu.sg>
2025-06-27 19:48:58 +0800

    Update router.js
    Added Upcoming Events

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Login from './views/Login.vue'
 import AttendanceDashboard from './views/AttendanceDashboard.vue'
 import SignUp from './views/Signup.vue'
 import CheckIn from './views/CheckIn.vue'
+import UpcomingEvents from './views/UpcomingEvents.vue'
 
 const routes = [
   { path: '/', component: Login },
   { path: '/dashboard', component: AttendanceDashboard },
   { path: '/signup', component: SignUp},
-  { path: '/checkin', component: CheckIn}
+  { path: '/checkin', component: CheckIn},
+  { path: '/upcomingevents', component: UpcomingEvents}
 ]
 
 export const router = createRouter({
